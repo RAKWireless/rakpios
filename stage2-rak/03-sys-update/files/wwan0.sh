1159e5aa412b8c59fbd8f0b7ec1b0f014fcbd2ed
Xose Pérez <xose.perez@gmail.com>
2023-03-27 16:13:20 +0200

    Add checks to AP and WWAN scripts

--- stage2-rak/03-sys-update/files/wwan0.sh
+++ stage2-rak/03-sys-update/files/wwan0.sh
@@ -3,11 +3,12 @@
 interface=$1
 event=$2
 
-if [[ "$interface" != "wwan0" ]] || [[ $event != "up" ]]
-then
-    return 0
+if [[ "$interface" != "wwan0" ]] || [[ $event != "up" ]]; then
+    exit 0
 fi
 
-ifconfig wwan0 down
-echo 1 > /sys/class/net/wwan0/qmi/raw_ip
-ifconfig wwan0 up
+if [[ -f /sys/class/net/wwan0/qmi/raw_ip ]]; then
+    ifconfig wwan0 down
+    echo 1 > /sys/class/net/wwan0/qmi/raw_ip
+    ifconfig wwan0 up
+fi

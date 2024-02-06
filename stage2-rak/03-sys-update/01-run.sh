c7374d2ccb60cd93db525ea6a8a60a60fe8e1842
Xose Pérez <xose.perez@gmail.com>
2024-02-06 22:28:02 +0100

    Using atecc-util to interface the ATECC608 chip

--- stage2-rak/03-sys-update/01-run.sh
+++ stage2-rak/03-sys-update/01-run.sh
@@ -1,26 +1,13 @@
 #!/bin/bash -e
 
-# Add rakuid
-cp files/rc.local "${ROOTFS_DIR}/etc/rc.local"
-cp files/rakuid "${ROOTFS_DIR}/bin/rakuid"
-cp files/get_RAKUID "${ROOTFS_DIR}/bin/get_RAKUID"
-
-#Add oled script
-cp files/oled "${ROOTFS_DIR}/bin/oled"
-
-# Add portainer up script
-cp files/portainer "${ROOTFS_DIR}/bin/portainer"
-
-# Add rakpios-cli
-on_chroot << EOF
-runuser -l ${FIRST_USER_NAME} -c 'curl https://raw.githubusercontent.com/RAKWireless/rakpios-cli/main/rakpios-cli -sSf | bash -s -- --install --silent'
-EOF
+# Add proper resize partition to firstboot script
+sed -i "s|^  fix_partuuid|  fix_partuuid\n  raspi-config --expand-rootfs\n  raspi-config nonint do_i2c 0\n|" "${ROOTFS_DIR}/usr/lib/raspberrypi-sys-mods/firstboot"
 
 # Update config.txt
-cp files/config.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/config.txt "${ROOTFS_DIR}/boot/firmware/"
 
 # Enable modules
-cp files/modules "${ROOTFS_DIR}/etc/modules"
+install -m 644 files/modules "${ROOTFS_DIR}/etc/"
         
 # Update os-release file
 cat > "${ROOTFS_DIR}/etc/os-release" << EOL
@@ -38,7 +25,36 @@ COMMIT_DATE="${COMMIT_DATE:-$(git log -1 --format=%ad --date=short)}"
 IMAGE_DATE="$(date +%Y-%m-%d)"
 EOL
 
-# add create-AP service
+# Force user to change password after first login
+on_chroot << EOF
+passwd -e $FIRST_USER_NAME
+echo "$FIRST_USER_NAME ALL=(ALL) PASSWD: ALL" > /etc/sudoers.d/010_pi-nopasswd
+EOF
+
+# Set wwan0 to raw IP mode when using BG96
+cp files/wwan0.sh "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/pre-up.d/"
+
+# Add rakuid utility
+cp files/atecc-util_0.4.11_arm64.deb "${ROOTFS_DIR}/tmp/"
+on_chroot << EOF
+apt install /tmp/atecc-util_0.4.11_arm64.deb
+EOF
+rm -f "${ROOTFS_DIR}/tmp/atecc-util_0.4.11_arm64.deb"
+install -m 755 files/rc.local "${ROOTFS_DIR}/etc/"
+install -m 755 files/rakuid "${ROOTFS_DIR}/usr/bin/"
+
+# Add oled script
+install -m 755 files/oled "${ROOTFS_DIR}/usr/bin/"
+
+# Add portainer up script
+install -m 755 files/portainer "${ROOTFS_DIR}/usr/bin/"
+
+# Add rakpios-cli
+on_chroot << EOF
+runuser -l ${FIRST_USER_NAME} -c 'curl https://raw.githubusercontent.com/RAKWireless/rakpios-cli/main/rakpios-cli -sSf | bash -s -- --install --silent'
+EOF
+
+# Add create_ap service
 cp files/create-ap.service "${ROOTFS_DIR}/etc/systemd/system/"
 cp files/create-ap "${ROOTFS_DIR}/usr/local/bin/"
 tar xvzf files/wifi-connect-v4.4.6-linux-aarch64-rakwireless.tar.gz -C files/
@@ -52,16 +68,7 @@ systemctl daemon-reload
 systemctl enable create-ap
 EOF
 
-
-# Force user to change password after first login
-on_chroot << EOF
-passwd -e $FIRST_USER_NAME
-echo "$FIRST_USER_NAME ALL=(ALL) PASSWD: ALL" > /etc/sudoers.d/010_pi-nopasswd
-EOF
-
 # Update MOTD
 cp files/update-motd.d/* "${ROOTFS_DIR}/etc/update-motd.d/"
 rm -rf "${ROOTFS_DIR}/etc/update-motd.d/10-uname"
 
-# Set wwan0 to raw IP mode when using BG96
-cp files/wwan0.sh "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/pre-up.d/"

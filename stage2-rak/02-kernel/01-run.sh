6741292cfb2b200b6954e1927683c755a7a914d2
Xose Pérez <xose.perez@gmail.com>
2024-10-29 18:11:50 +0100

    Cache firmware files

--- stage2-rak/02-kernel/01-run.sh
+++ stage2-rak/02-kernel/01-run.sh
@@ -70,17 +70,4 @@ fi
 popd >> /dev/null
 
 # Intel WiFi6 drivers (AX200, AX201 & AX210)
-# https://www.intel.com/content/www/us/en/support/articles/000005511/wireless.html
-wget https://wireless.wiki.kernel.org/_media/en/users/drivers/iwlwifi-ty-59.601f3a66.0.tgz
-wget https://wireless.wiki.kernel.org/_media/en/users/drivers/iwlwifi/iwlwifi-qu-48.13675109.0.tgz
-wget https://wireless.wiki.kernel.org/_media/en/users/drivers/iwlwifi/iwlwifi-cc-46.3cfab8da.0.tgz
-wget https://anduin.linuxfromscratch.org/sources/linux-firmware/intel/ibt-0041-0041.sfi
-wget https://anduin.linuxfromscratch.org/sources/linux-firmware/intel/ibt-0041-0041.ddc
-
-mkdir firmware
-for package in `ls *.tgz`; do 
-    tar -xzf $package -C firmware/
-done
-cp firmware/*/iwlwifi-*.ucode "${ROOTFS_DIR}/lib/firmware"
-cp ibt-0041-0041.* "${ROOTFS_DIR}/lib/firmware/intel"
-rm -rf iwlwifi* firmware ibt-0041-0041.*
+cp -r files/firmware/* "${ROOTFS_DIR}/lib/firmware/"

cbb06bdd670bccdb789c4d9264b760f75ea3046a
Xose Pérez <xose.perez@gmail.com>
2026-07-30 15:16:12 +0200

    Utils: prebuilt OLED binary, reset_password and hardware test suite

--- stage2-rak/03-utils/01-run.sh
+++ stage2-rak/03-utils/01-run.sh
@@ -5,13 +5,17 @@ install -m 755 files/wwan0.sh "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/pre
 
 # Add oled script
 install -m 644 files/oled.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 755 files/oled "${ROOTFS_DIR}/usr/local/bin/"
 on_chroot << EOF
+curl -L -o /usr/local/bin/oled https://github.com/xoseperez/rak7391-oled-c/releases/download/v2.0.0/oled-arm64-static
+chmod +x /usr/local/bin/oled
 systemctl enable oled
 EOF
 
+# Add reset_password utility
+install -m 755 files/reset_password "${ROOTFS_DIR}/usr/local/bin/"
+
 # Add portainer up script
-install -m 755 files/portainer "${ROOTFS_DIR}/usr/local/bin/"
+#install -m 755 files/portainer "${ROOTFS_DIR}/usr/local/bin/"
 
 # Add rakpios-cli
 on_chroot << EOF
@@ -21,6 +25,11 @@ EOF
 # Add mioty-cli
 on_chroot << EOF
 runuser -l ${FIRST_USER_NAME} -c 'curl https://raw.githubusercontent.com/RAKWireless/mioty-cli/master/mioty-cli -sSf | bash -s -- install'
+EOF
+
+# Add rak739x-hardware-test
+on_chroot << EOF
+runuser -l ${FIRST_USER_NAME} -c 'cd /home/${FIRST_USER_NAME}/.local; mkdir -p share; cd share ; rm -rf rak739x-hardware-test ; git clone  --recurse-submodules https://github.com/RAKWireless/rak739x-hardware-test'
 EOF
 
 # Add wisblock USB rules

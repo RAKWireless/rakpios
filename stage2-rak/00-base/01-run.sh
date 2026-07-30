1d784f97e6fd597c4bff06995ee034e6ef897aef
Xose Pérez <xose.perez@gmail.com>
2026-07-30 15:14:02 +0200

    RAK7391 overlay, cloud-init resize and raspi-config setup

--- stage2-rak/00-base/01-run.sh
+++ stage2-rak/00-base/01-run.sh
@@ -1,17 +1,37 @@
 #!/bin/bash -e
 
+# Cloud-Init for trixie (filesystem expansion on first boot)
+install -v -m 644 files/meta-data "${ROOTFS_DIR}/boot/firmware/meta-data"
+install -v -m 644 files/user-data "${ROOTFS_DIR}/boot/firmware/user-data"
+# network-config intentionally not installed (conflicts with NetworkManager)
+
 # Add custom firstboot script
-install -m 755 files/firstboot-rak "${ROOTFS_DIR}/usr/bin/"
-install -d "${ROOTFS_DIR}/usr/share/firstboot.d/"
-install -m 755 files/firstboot.d/* "${ROOTFS_DIR}/usr/share/firstboot.d/"
-sed -i "s|main$|main\nfirstboot-rak\n|" "${ROOTFS_DIR}/usr/lib/raspberrypi-sys-mods/firstboot"
+#install -m 755 files/firstboot-rak "${ROOTFS_DIR}/usr/bin/"
+#install -d "${ROOTFS_DIR}/usr/share/firstboot.d/"
+#install -m 755 files/firstboot.d/* "${ROOTFS_DIR}/usr/share/firstboot.d/"
+#sed -i "s|main$|main\nfirstboot-rak\n|" "${ROOTFS_DIR}/usr/lib/raspberrypi-sys-mods/firstboot"
+
+# Build overlays
+dtc -I dts -O dtb files/rak7391.dts -o files/rak7391.dtbo
+install -m 755 files/rak7391.dtbo "${ROOTFS_DIR}/boot/firmware/overlays/"
 
 # Update config.txt
 install -m 755 files/config.txt "${ROOTFS_DIR}/boot/firmware/"
 
+# Enable SSH
+on_chroot << EOF
+sudo raspi-config nonint do_ssh 0
+EOF
+
 # Enable I2C
 on_chroot << EOF
 echo "i2c-dev" >> /etc/modules
+sudo raspi-config nonint do_i2c 0
+EOF
+
+# Enable SPI
+on_chroot << EOF
+sudo raspi-config nonint do_spi 0
 EOF
 
 # Force user to change password after first login
@@ -23,6 +43,11 @@ EOF
 # Configure Network Manager
 on_chroot << EOF
 sed -i "s/managed=false/managed=true/g" "/etc/NetworkManager/NetworkManager.conf"
+EOF
+
+# Enable Wifi
+on_chroot << EOF
+raspi-config nonint do_wifi_country GB
 EOF
 
 # Update os-release file
